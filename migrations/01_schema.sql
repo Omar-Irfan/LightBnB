0c0af33a25a1b3cad48b83f34bd940a0f6ddd46a
Omar Irfan <omar.irfan18@gmail.com>
2021-04-29 20:17:24 +0000

    add property

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE properties (
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
 
-  active BOOLEAN NOT NULL DEFAULT TRUE
+  active BOOLEAN NOT NULL DEFAULT true
 );
 
 CREATE TABLE reservations (
